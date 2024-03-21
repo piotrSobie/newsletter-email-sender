53338ca6fcbd521c0b6116614da6730fe7e48f23
Piotr Sobiegraj <psobiegraj@sii.pl>
2024-03-21 18:05:08 +0100

    initial commit

---
+++ src/main/java/com/example/newsletteremailsender/NewsletterEmailSenderApplication.java
@@ -0,0 +1,13 @@
+package com.example.newsletteremailsender;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsletterEmailSenderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewsletterEmailSenderApplication.class, args);
+	}
+
+}
